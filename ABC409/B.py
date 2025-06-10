ce7e390d91b39bc755c9f262f58ebde8b65adccf
waiorecchi <tatukikitamura123456@gmail.com>
2025-06-11 00:07:05 +0900

    ss

---
+++ ABC409/B.py
@@ -0,0 +1,20 @@
+#7
+#1 6 2 10 2 3 2
+N =int(input())
+li =list(map(int,input().split()))
+ans =[]
+
+li.sort()
+
+for i in li:
+    count = 0
+    for j in range(N):
+        if i <= li[j]:
+                count += 1
+    if i <= count:
+        ans.append(i)
+    else:
+         break
+    
+print(max(ans))
+
